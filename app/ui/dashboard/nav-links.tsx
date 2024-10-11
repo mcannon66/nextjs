4470188b7bc0e769b8502289da03f31a83ddd324
Matthew B Cannon <matthew.cannon@ffwagency.com>
2024-10-11 10:11:15 -0400

    Fixed typescript error.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import Link, { Route } from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
@@ -30,7 +30,7 @@ export default function NavLinks() {
         return (
           <Link
             key={link.name}
-            href={link.href}
+            href={link.href as Route}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
